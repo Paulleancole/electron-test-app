52be94c5eae7eb06cdc3cf552f803a32305dfbb9
paul <paul_mlc@hotmail.com>
2025-01-22 01:14:10 -0300

    original

--- src/__tests__/app.spec.js
+++ src/__tests__/app.spec.js
@@ -1,24 +1,42 @@
 const { test, expect, _electron: electron } = require('@playwright/test')
-const path = require('path'); // Import the path module
-// const { _electron: electron } = require('playwright'); // Adjust import as needed
 
 test('an h1 contains hello world"', async () => {
-  console.log("pathhhh",path.join(__dirname, '../index.js'));
-  const electronApp = await electron.launch({ 
-    // args: [path.join(__dirname)],
-    args: [path.join(__dirname, '../index.js')],
-    logger: console
-  })
+  const electronApp = await electron.launch({ args: ['.'] })
 
   // Wait for the first BrowserWindow to open
   const window = await electronApp.firstWindow()
 
   // Check for the presence of an h1 element with the text "hello"
   const headerElement = await window.$('h1')
-  const headerText = await headerElement.textContent();
-  console.log('loggg',headerText);
+  const headerText = await headerElement.textContent()
   expect(headerText).toBe("💖 Hello World!")
 
   // Close the app
   await electronApp.close()
 })
+
+
+// const { test, expect, _electron: electron } = require('@playwright/test')
+// const path = require('path'); // Import the path module
+// // const { _electron: electron } = require('playwright'); // Adjust import as needed
+
+// test('an h1 contains hello world"', async () => {
+//   console.log("pathhhh",path.join(__dirname, '../index.js'));
+//   const electronApp = await electron.launch({ 
+//     // args: [path.join(__dirname)],
+//     args: [path.join(__dirname, '../index.js')],
+//     logger: console
+//   })
+
+//   // Wait for the first BrowserWindow to open
+//   const window = await electronApp.firstWindow()
+
+//   // Check for the presence of an h1 element with the text "hello"
+//   const headerElement = await window.$('h1')
+//   const headerText = await headerElement.textContent();
+//   console.log('loggg',headerText);
+//   expect(headerText).toBe("💖 Hello World!")
+
+//   // Close the app
+//   await electronApp.close()
+// })
